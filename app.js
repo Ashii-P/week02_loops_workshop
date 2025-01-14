f978746e5d59f00de39d808201e606cb5c8d53e0
Aishah Perks <byaishah.altaf@gmail.com>
2025-01-14 10:04:28 +0000

    Did the first half of the loop workshop

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("Hello World!");
+
+for (let i = 0; i < 5; i++) {
+  console.log("Iteration", i);
+}
+
+let counter = 1;
+
+while (counter <= 5) {
+  console.log(counter);
+  counter++;
+}
+
+let keepLooping = true;
+let i = 0;
+while (keepLooping === true) {
+  console.log("Iteration", i);
+  keepLooping = Math.random() < 0.1;
+  i++;
+}
+//Looping over arrays
