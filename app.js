c38a6087dcc25470fb72cf2990a438a3da15faee
Aishah Perks <byaishah.altaf@gmail.com>
2025-01-14 10:19:38 +0000

    Did the looping over arrays and alternative syntax for loops for arrays.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log("Hello World!");
 
+//For Loops
 for (let i = 0; i < 5; i++) {
   console.log("Iteration", i);
 }
@@ -19,3 +20,37 @@ while (keepLooping === true) {
   i++;
 }
 //Looping over arrays
+
+const favouriteFoods = [
+  "Pho",
+  "Fried Chicken Burger",
+  "Turkish Meat Platter",
+  "Biria Tacos",
+  "Sushi",
+  "Roast Dinner",
+  "Mashed Potatoes",
+  "Pad Thaii",
+  "Oranges",
+  "Lychess",
+];
+for (let i = 0; i < favouriteFoods.length; i++) {
+  console.log(favouriteFoods[i]);
+}
+
+for (let food of favouriteFoods) {
+  console.log(food);
+}
+const numbers = [7, 19, 2, 28, 20];
+for (let number of numbers) {
+  console.log(number);
+}
+
+favouriteFoods.forEach(function (food) {
+  console.log(food);
+});
+
+favouriteFoods.forEach(function (food, index) {
+  console.log(index, food);
+});
+
+//Loop Workshop
